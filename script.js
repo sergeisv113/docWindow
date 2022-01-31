3f9655c7c548a5765d6c08e593193d42cc46f798
Sergei <sergeisv113@gmail.com>
2022-01-31 16:37:22 +0200

    '2'

--- script.js
+++ script.js
@@ -13,6 +13,23 @@ const height = box.scrollHeight;
 console.log(width, height);
 //при клике откр полностью окно прокрутки
 btn.addEventListener('click', () => {
-    // box.style.height = box.scrollHeight + 'px';
-    console.log(box.scrollTop);
-})
+    // box.style.height = box.scrollHeight + 'px';// инлайн стиль
+    console.log(box.scrollTop);//сколько прошли от верха
+});
+//получ координат страницы
+console.log(box.getBoundingClientRect());
+console.log(box.getBoundingClientRect().top);//50
+//опред стилей, кот есть у элемента
+const style = window.getComputedStyle(box);//стиль css
+console.log(style);
+console.log(style.display);//block
+//получ стилей псевдо эл
+// window.getComputedStyle(box);
+
+//получ колич "рх" отлист user
+// console.log(document.documentElement.clientWidth);//1600
+console.log(document.documentElement.scrollTop);//0- не пролистано
+window.scrollBy(0, 400);//скрол на 400 рх в низ от текущ позиции
+window.scrollTo(0, 400);// скрол на 400 рх в низ от top page
+
+
